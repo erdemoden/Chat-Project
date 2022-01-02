71aaa964f39f2340169bbd3371d75ccf05908307
erdemoden <erdemoden5@gmail.com>
2022-01-02 22:12:50 +0300

    aaaa

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const express = require('express');
+const path = require('path')
 const app = express();
 const cookieparser = require('cookie-parser');
 const db = require('./db')();
@@ -16,7 +17,7 @@ const { signedCookie } = require('cookie-parser');
 const io = socketio(server);
 server.listen(process.env.PORT||1998);
 app.set('view-engine','ejs');
-app.set('Views', __dirname + '/app/Views');
+app.set('Views', path.join(__dirname, 'Views'));
 app.use(express.static('images'));
 app.use(express.static('scripts'));
 app.use(express.json());
